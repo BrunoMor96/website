60f1ef9d9e1f66b15a68ec2cfabef5c1e836358d
Bruno <brunoisil9696@gmail.com>
2021-03-16 17:42:09 -0500

    first commit

---
+++ scripts/acordeon.js
@@ -0,0 +1,24 @@
+var elementOld = null;
+var openClass
+
+function toggleAccordion(element) {
+    content = element.querySelector(".accordion__tab__content");
+    check = element.querySelector(".icon_check")
+
+    if(elementOld != null){
+        elementOld.classList.remove(openClass);
+        contentOld = elementOld.querySelector(".accordion__tab__content");
+        contentOld.style.maxHeight = "0px";
+        check.style.transform = "rotate(-180deg)"
+    }
+
+    if(elementOld !== element){
+        element.classList.add(openClass);
+        content.style.maxHeight = content.scrollHeight + "px";
+        check.style.transform = "rotate(0)"
+        elementOld = element;
+    }else{
+        elementOld = null;
+        check.style.transform = "rotate(-180deg)"
+    }
+}
---
+++ scripts/swipper-options.js
@@ -0,0 +1,66 @@
+const swiper = new Swiper('.swiper1', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    250: {
+      slidesPerView: 1.1,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 16
+    }
+  }
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination3',
+    clickable: true,
+  },
+  // autoplay: {
+  // 			delay: 3000,
+  // 			disableOnInteraction: false,
+  // },
+});
+
+const swiper4 = new Swiper('.swiper4', {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination4',
+    clickable: true,
+  },
+  breakpoints: {
+    250: {
+      slidesPerView: 1.1,
+      spaceBetween: 20
+    },
+    767: {
+      slidesPerView: 5,
+      spaceBetween: 16
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next4',
+    prevEl: '.swiper-button-prev4',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
